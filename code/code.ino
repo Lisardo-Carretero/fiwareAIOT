78c83ac2df45c89b7cf1bac3dc12406bde29e404
Lisardo Carretero Colmenar <Lisardokill@hotmail.com>
2025-04-03 10:52:03 +0200

    Abstracción de loop
    - mover todo a métodos parametrizables.

--- code/code.ino
+++ code/code.ino
@@ -8,12 +8,7 @@ const char *password = "TU_PASSWORD";
 
 // Configuración de FIWARE Orion
 const char *orionEndpoint = "http://orion:1026/v2/entities";
-const char *fiwareService = "openiot";
 const char *fiwareServicePath = "/";
-
-// Datos de la entidad
-String entityId = "AIOT_ESP32";
-String entityType = "EstacionIOTInvernadero";
 
 float getTemperature()
 {
@@ -35,69 +30,48 @@ int getCO2()
 
 void setup()
 {
-    Serial.begin(96000);
-    WiFi.begin(ssid, password);
-
-    Serial.print("Conectando a Wi-Fi");
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(1000);
-        Serial.print(".");
-    }
-    Serial.println("\nConectado a Wi-Fi");
+    Serial.begin(9600);
+    // WiFi.begin(ssid, password);
+    //
+    // Serial.print("Conectando a Wi-Fi");
+    // while (WiFi.status() != WL_CONNECTED)
+    // {
+    //     delay(1000);
+    //     Serial.print(".");
+    // }
+    // Serial.println("\nConectado a Wi-Fi");
 }
 
-void loop()
+void buildJsonDocument(JsonDocument &doc, const String &placa, const String &estacion, const String &nombreSensor, const String &tipoSensor, float valor)
 {
+    doc["id"] = placa;
+    doc["type"] = estacion;
+
+    // Agregar datos de sensores
+    doc[nombreSensor]["type"] = tipoSensor;
+    doc[nombreSensor]["value"] = valor;
+}
+
+// Le pongo como default el path de la entidad, pero lo puedo cambiar al momento de llamar a la función
+void sendToFiware(const String &jsonOutput, const String &fiwareServicePath = "/")
+{
+    println("Enviando datos a FIWARE Orion...");
+    Serial.println("JSON con formato :");
+    Serial.println(jsonOutput);
+
     if (WiFi.status() == WL_CONNECTED)
     {
         HTTPClient http;
-
-        // Crear el JSON dinámicamente
-        StaticJsonDocument<1024> doc;
-        doc["id"] = entityId;
-        doc["type"] = entityType;
-
-        // Agregar datos de sensores
-        doc["temperature"]["type"] = "Number";
-        doc["temperature"]["value"] = getTemperature();
-
-        doc["relativeHumidity"]["type"] = "Number";
-        doc["relativeHumidity"]["value"] = getHumidity();
-
-        doc["CO2"]["type"] = "Number";
-        doc["CO2"]["value"] = getCO2();
-
-        // Agregar ubicación
-        doc["location"]["type"] = "geo:json";
-        JsonObject locationValue = doc["location"]["value"].to<JsonObject>();
-        locationValue["type"] = "Point";
-        JsonArray coordinates = locationValue.createNestedArray("coordinates");
-        coordinates.add(-99.133167); // Longitud
-        coordinates.add(19.434072);  // Latitud
-
-        // Agregar fecha y hora
-        doc["dateObserved"]["type"] = "DateTime";
-        doc["dateObserved"]["value"] = "2025-04-03T12:00:00Z"; // Cambiar por la hora actual si es necesario
-
-        // Serializar el JSON a un string
-        String payload;
-        serializeJson(doc, payload);
-
-        // Configurar la solicitud HTTP
         http.begin(orionEndpoint);
         http.addHeader("Content-Type", "application/json");
-        http.addHeader("Fiware-Service", fiwareService);
-        http.addHeader("Fiware-ServicePath", fiwareServicePath);
+        http.addHeader("fiware-service", fiwareServicePath);
 
-        // Enviar la solicitud POST
-        int httpResponseCode = http.POST(payload);
+        int httpResponseCode = http.POST(jsonOutput);
 
-        // Manejar la respuesta
         if (httpResponseCode > 0)
         {
             String response = http.getString();
-            Serial.println("Respuesta del servidor:");
+            Serial.println("Respuesta de FIWARE Orion:");
             Serial.println(response);
         }
         else
@@ -110,8 +84,38 @@ void loop()
     }
     else
     {
-        Serial.println("Wi-Fi no conectado");
+        Serial.println("Error: No conectado a Wi-Fi");
     }
+}
 
-    delay(60000); // Esperar 1 minuto antes de enviar nuevamente
+void loop()
+{
+    StaticJsonDocument<1024> doc;
+    // Datos de prueba
+    String placa = "AIOT_ESP32";
+    String estacion = "EstacionIOTInvernadero";
+    String nombreSensor = "temperature";
+    String tipoSensor = "Number";
+    float valor = getTemperature(); // Simulación de lectura de temperatura
+
+    // Llamar al método buildJsonDocument
+    buildJsonDocument(doc, placa, estacion, nombreSensor, tipoSensor, valor);
+
+    // Voy a simular varias llamadas pare verificar que estoy montando bien el JSON
+    buildJsonDocument(doc, placa, estacion, "sensor2", "tipo2", getHumidity());
+
+    buildJsonDocument(doc, placa, estacion, "sensor3", "tipo3", getTemperature());
+
+    buildJsonDocument(doc, placa, estacion, "sensor3", "tipo3", getCO2());
+
+    // Serializar el JSON a un string para imprimirlo
+    String jsonOutput;
+    serializeJson(doc, jsonOutput);
+
+    // Imprimir el JSON generado en el monitor serie
+
+    // Enviar el JSON a FIWARE Orion
+    sendToFiware(jsonOutput);
+
+    delay(1000);
 }
