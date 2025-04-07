e4c0de6b9d677796f4e4407b2baf7124577ea4e3
Lisardo Carretero Colmenar <Lisardokill@hotmail.com>
2025-04-08 00:00:20 +0200

    Codigo prueba emisor receptor

---
+++ receptor/receptor.ino
@@ -0,0 +1,38 @@
+#include <ArduinoJson.h>
+
+void setup()
+{
+    Serial.begin(9600);
+
+    Serial.println("Arduino Mega listo para recibir datos...");
+}
+
+void loop()
+{
+    // Verificar si hay datos disponibles desde el ESP32
+    if (Serial.available())
+    {
+        String jsonData = Serial.readStringUntil('\n'); // Leer los datos enviados por el ESP32
+
+        // Parsear el JSON recibido
+        StaticJsonDocument<128> doc;
+        DeserializationError error = deserializeJson(doc, jsonData);
+
+        if (error)
+        {
+            Serial.print("Error al parsear JSON: ");
+            Serial.println(error.c_str());
+            return;
+        }
+
+        // Extraer los valores del JSON
+        float temperature = doc["temperature"];
+        float humidity = doc["humidity"];
+
+        // Mostrar los datos en el monitor serie
+        Serial.print("Temperatura: ");
+        Serial.println(temperature);
+        Serial.print("Humedad: ");
+        Serial.println(humidity);
+    }
+}
